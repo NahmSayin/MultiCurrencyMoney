4783c7de393bfae3ec3ab77b22a815645ced6f75
Nahom Alem <alemnahom@outlook.com>
2022-07-21 15:41:49 +0100

    Unit test created(TDD)

---
+++ src/main/java/org/example/Dollar.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Dollar(int amount) {
+    void times(int multiplier){
+        int amount=10;
+    }
+}
---
+++ src/test/java/org/example/DollarTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.Test;
+
+public class DollarTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
